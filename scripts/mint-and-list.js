4917e2ee224da4d396292a29017029e133737428
haverstein <haverstein3@gmail.com>
2022-09-15 15:46:03 +0530

    mine-blocks-utils-added

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat")
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
 
 const price = ethers.utils.parseEther("0.1")
 
@@ -17,6 +18,10 @@ async function mintAndList() {
     const tx = await nftMarketplace.listItem(basicNft.address, tokenId, price)
     await tx.wait(1)
     console.log("Listed!")
+
+    if (network.config.chainId == "31337") {
+        await moveBlocks(2, (sleepAmount = 1000))
+    }
 }
 
 mintAndList()
---
+++ utils/move-blocks.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat")
+
+function sleep(timesInMs) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, timesInMs)
+    })
+}
+
+async function moveBlocks(amount, sleepAmount = 0) {
+    console.log("Moving blocks...")
+    for (let i = 0; i < amount; i++) {
+        await network.provider.request({
+            method: "evm_mine",
+            params: [],
+        })
+    }
+    if (sleepAmount) {
+        console.log(`Sleeping for ${sleepAmount}`)
+        await sleep(sleepAmount)
+    }
+}
+
+module.exports = {
+    moveBlocks,
+    sleep,
+}
